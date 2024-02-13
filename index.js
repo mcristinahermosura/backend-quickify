4482e2c583cbc9dfd4b2fd313a8a70465f4a0a69
mcristinahermosura <mcristinahermosura@gmail.com>
2024-02-13 12:38:40 +0800

    Fixing api deployed endpoint with logger

--- index.js
+++ index.js
@@ -20,12 +20,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(logger);
-app.use("/b4/users", userRoutes);
-app.use("/b4/products", productRoutes);
-app.use("/b4/orders", orderRoutes);
 
-app.use("/files", express.static(path.join(__dirname, "uploads")));
+app.use("/b4/users", logger, userRoutes);
+app.use("/b4/products", logger, productRoutes);
+app.use("/b4/orders", logger, orderRoutes);
+app.use("/b4/files", logger, express.static(path.join(__dirname, "uploads")));
 
 app.listen(PORT, () => {
   console.log(`E-commerce API is listening on port ${PORT}`);
